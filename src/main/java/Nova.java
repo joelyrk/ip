2fdb9eb48f75ab6b70fd1d66ed9475d106d10625
Joel <joelyrk04@gmail.com>
2026-08-20 03:10:39 +0800

    Add in-memory task storage and listing
    Store user-entered tasks for the duration of the session and support listing them in insertion order. This completes the Level-2 chatbot behavior without introducing persistence.

--- src/main/java/Nova.java
+++ src/main/java/Nova.java
@@ -19,6 +19,9 @@ public class Nova {
         System.out.println(separator);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
             System.out.println(separator);
@@ -29,7 +32,15 @@ public class Nova {
                 break;
             }
 
-            System.out.println(" " + command);
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println(" added: " + command);
+            }
             System.out.println(separator);
         }
     }
