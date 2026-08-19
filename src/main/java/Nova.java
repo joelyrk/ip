599e84e3bf7933d569adfee3da62ac4189f2a73a
Joel <joelyrk04@gmail.com>
2026-08-20 02:49:34 +0800

    Rename chatbot from Duke to Nova
    Align the entry-point class, startup banner, and user documentation with the selected product name.

---
+++ src/main/java/Nova.java
@@ -0,0 +1,13 @@
+/**
+ * Starts the Nova chatbot application.
+ */
+public class Nova {
+    public static void main(String[] args) {
+        String banner = " _   _                 \n"
+                + "| \\ | | _____   ____ _ \n"
+                + "|  \\| |/ _ \\ \\ / / _` |\n"
+                + "| |\\  | (_) \\ V / (_| |\n"
+                + "|_| \\_|\\___/ \\_/ \\__,_|\n";
+        System.out.println(banner);
+    }
+}
